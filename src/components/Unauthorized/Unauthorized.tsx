1d8bba74dfb9b1a483dce8e334ca52772c112057
Kapela <xavier.plein@kapela.fr>
2022-11-09 15:22:08 +0100

    Added user link for checking

--- src/components/Unauthorized/Unauthorized.tsx
+++ src/components/Unauthorized/Unauthorized.tsx
@@ -1,19 +1,34 @@
-import React from "react";
-import {useNavigate} from "react-router-dom";
+import React, {useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Unauthorized() {
 	const navigate = useNavigate();
+	const [username, setUsername] = useState("");
+
+	function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+		setUsername(e.currentTarget.value.replace(" ", "").trim());
+	}
 
 	return (
 		<>
-			<div style={{display: "flex"}}>
+			<div>
 				<div>Unauthorized: Acces Denied !</div>
+				<div>
+					<input
+						type="text"
+						id="username"
+						placeholder="username"
+						onChange={(e) => handleInput(e)}
+						value={username}
+					/>
+					<Link to={"/user/" + username}>Visit User {username}</Link>
+				</div>
 				<button
 					onClick={() => {
 						navigate("/home");
 					}}
 				>
-					Back home
+					Go Back home
 				</button>
 			</div>
 		</>
